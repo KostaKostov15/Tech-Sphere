c8957e497f9255cfc12f8380473dfcc197cae427
KostaKostov15 <kosta.kostov15@gmail.com>
2023-12-07 23:30:28 +0200

    added tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'false',
   theme: {
-    extend: {},
+    fontFamily: {
+      raleway: ['Raleway'],
+    },
+    extend: {
+      gridTemplateRows: {
+        '[auto,auto,1fr]': 'auto auto 1fr',
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/aspect-ratio')],
 };
