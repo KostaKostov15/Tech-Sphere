539466b9600188fc838a0a4813e7ed201949ae02
KostaKostov15 <kosta.kostov15@gmail.com>
2023-12-14 00:25:05 +0200

    added Cart component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,6 +16,7 @@ import {
   TransitionRoot,
 } from '@headlessui/vue';
 import { Bars3Icon, MagnifyingGlassIcon, ShoppingBagIcon, XMarkIcon } from '@heroicons/vue/24/outline';
+import Cart from '../components/Cart.vue';
 
 const navigation = {
   categories: [
@@ -141,6 +142,11 @@ const navigation = {
 };
 
 const isOpen = ref(false);
+const isCartOpen = ref(false);
+
+function toggleCart() {
+  isCartOpen.value = !isCartOpen.value;
+}
 </script>
 
 <template>
@@ -357,12 +363,13 @@ const isOpen = ref(false);
 
               <!-- Cart -->
               <div class="ml-4 flow-root lg:ml-6">
-                <a href="#" class="group -m-2 flex items-center p-2">
+                <a class="group -m-2 flex items-center p-2" @click="toggleCart">
                   <ShoppingBagIcon class="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" aria-hidden="true" />
                   <span class="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
                   <span class="sr-only">items in cart, view bag</span>
                 </a>
               </div>
+              <Cart v-if="isCartOpen" />
             </div>
           </div>
         </div>
--- src/configs/router.js
+++ src/configs/router.js
@@ -3,10 +3,13 @@ import Home from '../views/Home.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
 
+// import Cart from '../components/Cart.vue';
+
 const routes = [
   { path: '/', component: Home },
   { path: '/users/login', component: Login },
   { path: '/users/register', component: Register },
+  // { path: '/cart', component: Cart },
 ];
 
 const router = createRouter({
