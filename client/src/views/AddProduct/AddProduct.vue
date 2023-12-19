6da97c5273a4c93d275ea58e2d0034b563587942
KostaKostov15 <kosta.kostov15@gmail.com>
2023-12-20 00:06:09 +0200

    Update AddProduct.vue

--- client/src/views/AddProduct/AddProduct.vue
+++ client/src/views/AddProduct/AddProduct.vue
@@ -225,7 +225,7 @@ async function submitHandler() {
     </div>
 
     <div class="mt-6 flex items-center justify-end gap-x-6">
-      <button type="button" class="text-sm font-semibold leading-6 text-gray-900">
+      <button type="button" class="text-sm font-semibold leading-6 text-gray-900" @click="router.push(paths.home)">
         Cancel
       </button>
       <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
